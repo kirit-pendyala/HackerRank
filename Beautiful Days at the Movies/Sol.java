49213b3361ea02089be4f3343f79305984faeb6f
kirit-pendyala <pendyalakirit@gmail.com>
2021-02-11 10:16:53 -0600

    Solution

---
+++ Beautiful Days at the Movies/Sol.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the beautifulDays function below.
+
+    static int beautifulDays(int i, int j, int k)
+    {
+        int r = 0;
+        int sum  = 0;
+        int count = 0;
+        for (int s = i, c = i; c <= j; c++, s++)
+        {
+            s = c;
+            while (s > 0)
+            {
+                r = s % 10;
+                sum = sum * 10 + r;
+                s /= 10;
+            }
+
+            if ( (c - sum) % k == 0)
+                count++;
+
+            sum = 0;
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] ijk = scanner.nextLine().split(" ");
+
+        int i = Integer.parseInt(ijk[0]);
+
+        int j = Integer.parseInt(ijk[1]);
+
+        int k = Integer.parseInt(ijk[2]);
+
+        int result = beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
